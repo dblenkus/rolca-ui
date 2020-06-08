9b3e1b196e1a0887c9d9be07b852f505206d6aa9
Domen Blenkuš <domen@blenkus.com>
2020-06-08 19:09:23 +0200

    Use relative url for api

--- src/services/Base.ts
+++ src/services/Base.ts
@@ -4,7 +4,7 @@ import store from '../store/index';
 import { addNotificationError } from '../store/notifications/actions';
 
 let config: AxiosRequestConfig = {
-    baseURL: 'http://localhost:8000/api/v1',
+    baseURL: '/api/v1',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
