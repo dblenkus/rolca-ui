53cbf441fe78c9d11546cac4ee04f22b818a63fb
Domen Blenkuš <domen@blenkus.com>
2020-10-30 22:33:35 +0100

    Remove non-asci characters from filenames for upload

--- src/services/ImageService.ts
+++ src/services/ImageService.ts
@@ -5,10 +5,12 @@ import { Image } from '../types/api';
 
 export default {
     uploadImage(image: File): AxiosPromise<Image> {
+        // eslint-disable-next-line no-control-regex
+        const filename = image.name.replace(/[^\u000A\u0020-\u007E]/g, ' ');
         return apiClient.post('/file', image, {
             headers: {
                 'Content-Type': image.type,
-                'Content-Disposition': `attachment; filename=${image.name}`,
+                'Content-Disposition': `attachment; filename=${filename}`,
             },
         });
     },
